3c159f283ec7198827e07c3cf54ad4f96c0ce080
cloudprofessionals <mycloudprofessionals@gmail.com>
2021-04-18 16:55:47 -0400

    added instructions

--- terraform/etl/main.tf
+++ terraform/etl/main.tf
@@ -4,15 +4,15 @@ provider "aws" {
   region = var.region
 }
 #set the backend for state file
-terraform {
-  backend "s3" {
-    bucket = "aws-etl-demo-bucket"
-    key = "resources/terraform.tfstate"
-    workspace_key_prefix = "env"
-    region = "us-east-1"
-    encrypt = true
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "aws-etl-demo-bucket"
+#    key = "resources/terraform.tfstate"
+#    workspace_key_prefix = "env"
+#    region = "us-east-1"
+#    encrypt = true
+#  }
+#}
 
 
 
